1774bbc859a78260959bce06863cdb1951df0f3e
Ben Cosmin <94783151+Ben-C3012@users.noreply.github.com>
2022-04-14 11:01:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,52 +31,41 @@ questions.forEach( question => {
 
 
 
-
-// questions[0].addEventListener('click', ()=> {
-//      answers[0].classList.toggle('active')
-//      icons[0].classList.toggle('active')
-//      para[0].classList.toggle('active')
-//     questionHeaders[0].classList.toggle('active')
-
-// })
+questions[1].addEventListener('click', ()=> {
+    answers[1].classList.toggle('active')
+    icons[1].classList.toggle('active')
+    para[1].classList.toggle('active')
+    questionHeaders[1].classList.toggle('active')
 
 
+})
 
-// questions[1].addEventListener('click', ()=> {
-//     answers[1].classList.toggle('active')
-//     icons[1].classList.toggle('active')
-//     para[1].classList.toggle('active')
-//     questionHeaders[1].classList.toggle('active')
+questions[2].addEventListener('click', ()=> {
+    answers[2].classList.toggle('active')
+    icons[2].classList.toggle('active')
+    para[2].classList.toggle('active')
+    questionHeaders[2].classList.toggle('active')
+})
 
+})
 
-// })
-
-// questions[2].addEventListener('click', ()=> {
-//     answers[2].classList.toggle('active')
-//     icons[2].classList.toggle('active')
-//     para[2].classList.toggle('active')
-//     questionHeaders[2].classList.toggle('active')
-// })
-
-// })
-
-// questions[3].addEventListener('click', ()=> {
-//     answers[3].classList.toggle('active')
-//     icons[3].classList.toggle('active')
-//     para[3].classList.toggle('active')
-//     questionHeaders[3].classList.toggle('active')
+questions[3].addEventListener('click', ()=> {
+    answers[3].classList.toggle('active')
+    icons[3].classList.toggle('active')
+    para[3].classList.toggle('active')
+    questionHeaders[3].classList.toggle('active')
 
 
-// })
+})
 
-// questions[4].addEventListener('click', ()=> {
-//     answers[4].classList.toggle('active')
-//     icons[4].classList.toggle('active')
-//     para[4].classList.toggle('active')
-//     questionHeaders[4].classList.toggle('active')
+questions[4].addEventListener('click', ()=> {
+    answers[4].classList.toggle('active')
+    icons[4].classList.toggle('active')
+    para[4].classList.toggle('active')
+    questionHeaders[4].classList.toggle('active')
 
 
-// })
+})
 
 
 
